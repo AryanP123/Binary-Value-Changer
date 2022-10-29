32ce081a1cdbb3f802336b10246136f978746680
Aryan Puttur <36793614+AryanP123@users.noreply.github.com>
2022-10-28 22:13:14 -0400

    Create ReverseLinkedList.c

---
+++ ReverseLinkedList.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node;
+typedef struct node Node;
+
+struct node
+{
+    int data;
+    Node* next;
+};
+
+//declare your function here.
+Node* reverse_linked_list(Node* head);
+
+
+int main(int argc, char* argv[])
+{
+    Node* head = NULL;
+    int i;
+    Node* temp;
+
+    //set up a test list with values 9->8->7->...->0
+    for (i = 0; i < 10; i++)
+    {
+        temp = (Node*)malloc(sizeof(Node));
+        if (temp == NULL)
+        {
+            printf("out of memory?\n");
+            exit(1);
+        }
+        temp->data = i;
+        temp->next = head;
+        head = temp;
+    }
+
+    //call your function to reverse the list (should work for any list given the head node pointer).
+    head = reverse_linked_list(head);
+    //print the reversed list.
+    temp = head;
+    while (temp != NULL)
+    {
+        printf("%d\n", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
+
+
+//Define your function here
+Node* reverse_linked_list(Node* head){
+    Node* previous = NULL;
+    Node* next = head;
+    while(head != NULL){
+        next = next->next;
+        head->next = previous;
+        previous = head;
+        head = next;
+    }
+    return previous;
+}
+
